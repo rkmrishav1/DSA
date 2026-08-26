b32933d5f53782f3008ea2799a3a73a32734ea0c
rkmrishav1 <rkrishavmishra@gmail.com>
2026-08-27 01:32:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.java
@@ -0,0 +1,16 @@
+
+
+
+class Solution {
+    public static int largest(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
